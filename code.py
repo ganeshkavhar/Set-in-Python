97d338a463f849667cddf7a9c0f6380a2c89d3be
ganesh kavhar <kavharg@gmail.com>
2023-10-02 10:29:57 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+# Python Sets
+Sets in python are an unordered collection of data
+A typical set in python is of the format: {a,b,c}
+Sets are MUTABLE
+All the elements of a set is unique i.e. sets do not allow duplicates
+Cannot add list datatype as an element
+Frozen Sets: IMMUTABLE
+Some commonly used functions on sets: .add(), .update(), .remove(), .discard()
